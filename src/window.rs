51ee8ab947db59ec84d428f8f02245ddc0d14e7b
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-14 16:37:42 -0300

    feat: officially rendering the firts 3d model!

--- src/window.rs
+++ src/window.rs
@@ -74,6 +74,14 @@ impl Framebuffer {
         self.data[x + y * self.width] = value;
     }
 
+    pub fn set_pixel_f32(&mut self, x: usize, y: usize, value: f32) {
+        self.data[y * self.width + x] = (value * u32::MAX as f32) as u32;
+    }
+
+    pub fn get_pixel_f32(&mut self, x: usize, y: usize) -> f32 {
+        self.data[y * self.width + x] as f32 / u32::MAX as f32
+    }
+
     pub fn clear(&mut self, value: u32) {
         for i in 0..self.data.len() {
             self.data[i] = value;
