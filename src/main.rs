434551cfbb77422846c8d37721d74817e94b21f5
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-14 16:14:16 -0300

    feat: defining the Model

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use glam::*;
 mod window;
 use window::{Window, Framebuffer};
 
+mod model;
+use model::*;
+
 fn from_u8_rgb(r: u8, g: u8, b: u8) -> u32 {
     let (r, g, b) = (r as u32, g as u32, b as u32);
     (r << 16) | (g << 8) | b
