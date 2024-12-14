26e537badacd631b314126ef826ff28503f70495
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-14 15:11:42 -0300

    feat: new framebuffer to display the window

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 mod window;
 use window::Window;
 
+fn from_u8_rgb(r: u8, g: u8, b: u8) -> u32 {
+    let (r, g, b) = (r as u32, g as u32, b as u32);
+    (r << 16) | (g << 8) | b
+}
+
 fn main() {
-    let window: Window = Window::new("Motley project", 512, 512);
+    let mut window: Window = Window::new("Motley project", 512, 512);
 
     while !window.should_close() {
+        let framebuffer = window.framebuffer();
+
+        for x in 0..framebuffer.width(){
+            for y in 0..framebuffer.height(){
+                framebuffer.set_pixel(x, y, from_u8_rgb(20, 20, 20));
+            }
+        }
         
+        window.display();
     }
+
+
 }
--- src/window.rs
+++ src/window.rs
@@ -1,9 +1,16 @@
 pub struct Window {
-    window: minifb::Window
+    window: minifb::Window,
+    framebuffer: Framebuffer
+}
+
+pub struct Framebuffer {
+    data: Vec<u32>,
+    width: usize,
+    height: usize
 }
 
 impl Window {
-    pub fn new(name: &str, width: usize, height: usize) -> Self{
+    pub fn new(name: &str, width: usize, height: usize) -> Self {
         let options = minifb::WindowOptions {
             resize: true,
             ..Default::default()
@@ -16,12 +23,55 @@ impl Window {
             options
         ).expect("Failed to create window.");
 
+        let framebuffer = Framebuffer::new(width, height);
+
         Window {
-            window
+            window,
+            framebuffer
         }
     }
 
-    pub fn should_close(&self) -> bool{
+    pub fn should_close(&self) -> bool {
         !self.window.is_open()
     }
+
+    pub fn display(&mut self) {
+        self.window.update_with_buffer(
+            &self.framebuffer.data,
+            self.framebuffer.width(),
+            self.framebuffer.height()
+        ).expect("Failed to update window buffer.");
+
+        let (width, height) = self.window.get_size();
+        if width != self.framebuffer.width() || height != self.framebuffer.height() {
+            self.framebuffer = Framebuffer::new(width, height);
+        }
+    }
+
+    pub fn framebuffer(&mut self) -> &mut Framebuffer {
+        &mut self.framebuffer
+    }
+}
+
+impl Framebuffer {
+    pub fn new(width: usize, height: usize) -> Self {
+        Framebuffer {
+            data: vec![0; width * height],
+            width,
+            height
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn set_pixel(&mut self, x: usize, y: usize, value: u32) {
+        self.data[x + y * self.width] = value;
+    }
+
 }
