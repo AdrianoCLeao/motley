c8aab5dbcbe5cbe04add560a6866a4782ef71f26
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-15 15:25:09 -0300

    feat: implementing light

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,6 @@
 use glam::*;
+use crate::{Texture, load_texture};
+use std::path::Path;
 
 /*
 The `Vertex` struct represents a single vertex in a 3D mesh. It includes position and normal
@@ -8,14 +10,16 @@ a default vertex with zeroed position and normal.
 #[derive(Clone, Copy, Debug)]
 pub struct Vertex {
     pub position: Vec3,
-    pub normal: Vec3
+    pub normal: Vec3,
+    pub tex_coord: Vec2
 }
 
 impl Default for Vertex {
     fn default() -> Self {
         Vertex {
             position: Vec3::ZERO,
-            normal: Vec3::ZERO
+            normal: Vec3::ZERO,
+            tex_coord: Vec2::ZERO
         }
     }
 }
@@ -35,15 +39,17 @@ pub struct Mesh {
 The `Material` struct defines the appearance of a mesh using a base color stored as a `Vec4`.
 The `Default` trait initializes it with a white color.
 */
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 pub struct Material {
-    pub base_color: Vec4
+    pub base_color: Vec4,
+    pub base_color_texture: Option<Texture>
 }
 
 impl Default for Material {
     fn default() -> Self {
         Material {
-            base_color: Vec4::ONE
+            base_color: Vec4::ONE,
+            base_color_texture: None
         }
     }
 }
@@ -67,7 +73,8 @@ fn process_node(
     node: &gltf::Node,
     buffers: &[gltf::buffer::Data],
     meshes: &mut Vec<Mesh>,
-    materials: &mut [Material]
+    materials: &mut [Material],
+    file_path: &str
 ) {
     if let Some(mesh) = node.mesh() {
         for primitive in mesh.primitives() {
@@ -99,6 +106,12 @@ fn process_node(
                     }
                 }
 
+                if let Some(tex_coords) = reader.read_tex_coords(0) {
+                    for (i, tex_coord) in tex_coords.into_f32().enumerate() {
+                        vertices[i].tex_coord = Vec2::from(tex_coord);
+                    }
+                }
+
                 let indices = reader
                     .read_indices()
                     .map(|read_indices| {
@@ -111,6 +124,15 @@ fn process_node(
 
                 let material = &mut materials[material_idx];
                 material.base_color = Vec4::from(pbr.base_color_factor());
+                if let Some(base_color_texture) = pbr.base_color_texture() {
+                    if let gltf::image::Source::Uri { uri, .. } = base_color_texture.texture().source().source() {
+                        let model_path = Path::new(file_path);
+                        let texture_path = model_path.parent().unwrap_or_else(|| Path::new("./")).join(uri);
+                        let texture_path_str = texture_path.into_os_string().into_string().unwrap();
+
+                        material.base_color_texture = Some(load_texture(&texture_path_str));
+                    }
+                }
 
                 meshes.push(Mesh {
                     vertices,
@@ -141,7 +163,8 @@ pub fn load_model(file_path: &str) -> Model {
             document.nodes().next().as_ref().unwrap(),
             &buffers,
             &mut meshes,
-            &mut materials
+            &mut materials,
+            file_path
         );
     }
 
