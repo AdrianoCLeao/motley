2209f18f4c43af3c2895f3260c3c9946e387e936
AdrianoCLeao <adrianocoelho133@gmail.com>
2024-12-20 11:16:03 -0300

    fix: fixing performance test to not generate a window

--- benches/performance.rs
+++ benches/performance.rs
@@ -1,15 +1,6 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use motley::{gui::Window, model::load_model};
 
-fn benchmark_window_creation(c: &mut Criterion) {
-    c.bench_function("Window creation", |b| {
-        b.iter(|| {
-            let mut window = Window::new("Benchmark Test", 512, 512);
-            window.display();
-        });
-    });
-}
-
 fn benchmark_model_loading(c: &mut Criterion) {
     c.bench_function("Model loading", |b| {
         b.iter(|| {
@@ -26,7 +17,7 @@ fn create_criterion() -> Criterion {
 criterion_group! {
     name = benches;
     config = create_criterion();
-    targets = benchmark_window_creation, benchmark_model_loading
+    targets = benchmark_model_loading
 }
 
 criterion_main!(benches);
